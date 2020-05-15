879c44b9a47b87c1148cbc8e78f3db705b3d3b51
moliver4 <tifftang22@gmail.com>
2020-05-14 17:51:39 -0700

    interval fix

--- shoe.js
+++ shoe.js
@@ -2,17 +2,27 @@
 const click = (id) => {
     const select = document.getElementById(id);
     if (select === null ) return;
-    select.selectedIndex = 4;
+    for(let i = 0;i < select.options.length;i++){
+        if(select.options[i].value == '10' ){
+            select.options[i].selected = true;
+        }
+    }
+    // select.selectedIndex = 4;
     console.log(select.value)
+
+    setTimeout(checkText, 100)
+}
+const checkText=()=>{    
     const text = document.getElementById("AddToCartText")
     if (text.textContent !== 'Sold Out') {
-        clearInterval(interval)
+        // clearInterval(interval)
         const addButton = document.getElementById("AddToCart")
         addButton.disabled = false
         addButton.click()
         console.log('Added to Cart')      
-        setTimeout(clickCart, 500);
+        setTimeout(clickCart, 300);
     } else {
+        console.log('reloading')
         setTimeout(location.reload(), 1000);
     }
     
@@ -22,22 +32,23 @@ const clickCart = () => {
     document.getElementsByClassName("site-header__cart")[0].click();
     if (window.location.href.indexOf("cart") != -1) {
         location.reload();
-        clearInterval(interval)
+        // clearInterval(interval)
     }
 }
 
 const linkCheck = () => {
     if (window.location.href.indexOf("products") == -1) {
         console.log('stopped here')
-        clearInterval(interval)
+        // clearInterval(interval)
         return;
     }
-    click("ProductSelect-product-template-option-0")
     console.log('on product page')
+    click("ProductSelect-product-template-option-0")
+    
 }
 
 
 // setTimeout(click("ProductSelect-product-template-option-0"), 300)
 
-const interval = setInterval(linkCheck, 300);
+setTimeout(linkCheck, 1000);
 //   console.log("here");
